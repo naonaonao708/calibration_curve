1ca180604420ad7b1faa43c509f574dc37d02e47
naonao708 <naonao708@outlook.com>
2022-12-12 17:15:43 +0900

    12/12

--- src/calib.R
+++ src/calib.R
@@ -5,3 +5,13 @@ library(ggsignif)
 
 
 setwd('/Users/kohara/Desktop/calibration_curve/src')
+data <- read.xlsx('../data/22.12.10.xlsx')
+
+master_con <- 1.4 # μg/μl
+
+sample_name <- factor(data[0, ])
+
+abs <- c()
+for (i in range(1:3)) {
+    abs <- as.numeric(c(abs, data[i, ]))
+    }
